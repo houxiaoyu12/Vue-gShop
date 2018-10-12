95eb5ce4aa6b09f5206052c856368507d2f37c2a
houxiaoyu <1531604938@qq.com>
2018-10-12 10:15:00 +0800

    'update'

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-if="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+
+  export default {
+
+    mounted () {
+      //异步获取address，并保存到state中
+      this.$store.dispatch('getAddress')
+    },
+
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+//入口文件
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import TopHeader from './components/TopHeader/TopHeader.vue'
+import 'swiper/dist/css/swiper.min.css'
+
+// 注册全局组件
+Vue.component('TopHeader', TopHeader)
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+
+  /* components: { App },
+   template: '<App/>'
+   */
+  /*render: function (createElement) {  // 函数返回的标签会被渲染到el中
+    return createElement(App)   // '<App/>'
+  }*/
+  render: h => h(App),
+
+  router,
+  store
+})
